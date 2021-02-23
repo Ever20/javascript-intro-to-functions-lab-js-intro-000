c2fbeb6cc7bc7554369b95483be6ec6351d68a3f
Francisco trejo <f85.trejo@gmail.com>
2021-02-23 07:47:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ function logShout(string) {
   return console.log() {
          string.toUpperCase
   }
+function logWhisper(string) {
+  return console.log()
+          string.toLowerCase
+}
+function sayHiToGrandma(string) {
+  return string.toLowerCase("I can't hear you!")
+  return string.toUpperCase("YES INDEED!")
+  return string.("I love you, too.")
+  return string.("I love you, Grandma")
 }
